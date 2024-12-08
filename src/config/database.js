170ca6364b7d8a77c742891b4e9b86cd6cf62564
PaulCabreaEPN <paul.cabrera@epn.edu.ec>
2024-12-08 18:48:40 -0500

    first commit

---
+++ src/config/database.js
@@ -0,0 +1,18 @@
+import mongoose  from "mongoose";
+
+mongoose.set('strictQuery', true);
+
+const connection = async()=>{
+    try {
+        await mongoose.connect(process.env.MONGODB_URI_PRODUCTION, {
+        useNewUrlParser: true, 
+        useUnifiedTopology: true 
+        })
+        console.log(`Conexión exitosa a MongoDB Atlas`)
+    } catch (error) {
+        console.log("Error al conectar a MongoDB: ",error);
+    }
+};
+
+export default  connection
+
