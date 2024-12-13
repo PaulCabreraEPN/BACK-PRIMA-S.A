30a778140b74f75e70740adfae253fc5a826c856
IronEva2312 <agusmaty23@gmail.com>
2024-12-13 17:11:19 -0500

    Mejora del diccionario

--- src/helpers/passwordGenerator.js
+++ src/helpers/passwordGenerator.js
@@ -1,5 +1,5 @@
 const passwordGenerator = (length = 8) => {
-    const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()"
+    const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%&*()"
     let password = ""
 
     for (let i = 0; i < length; i++) {
